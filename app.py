2b3c26d974f28ab9b2a1dfa953def66751c139f3
Chiranjeev Singh <chiranjeevsingh140@gmail.com>
2022-01-15 21:48:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,14 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/app')
 def blog():
-    return "Hello, from Flask App!"
+    return "Hello, from App!"
 
 
 
 if __name__ == '__main__':
-    app.run(threaded=True,port=8081)
+    app.run(threaded=True,host='0.0.0.0',port=8081)
