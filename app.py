670583a49343da5cfbde094e90db33957747d07f
Chiranjeev Singh <chiranjeevsingh140@gmail.com>
2022-01-16 17:36:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,10 @@
-from flask import render_template
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/app')
-def blog():
-    return "Hello, from App!"
+def hello():
+	return "welcome to the flask tutorials"
 
 
-
-if __name__ == '__main__':
-    app.run(threaded=True,host='0.0.0.0',port=8081)
+if __name__ == "__main__":
+	app.run(host ='0.0.0.0', port = 5000, debug = True)
