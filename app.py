fe811c46821966c0327cf805c49ecec77a1682d4
Chiranjeev Singh <chiranjeevsingh140@gmail.com>
2022-04-09 14:46:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-	return "welcome to the new flask series..!!!!!!!"
+	return "welcome to the new flask series..!!!!!!!!"
 
 
 if __name__ == "__main__":
